f0efd94ea99e6ddd68c7052f1288f7e34643d5e3
Antonio Adami <antonioadamipalma@gmail.com>
2020-11-21 16:25:26 -0300

    Realização do desafio

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,22 @@ interface Balance {
   total: number;
 }
 
+interface CreateAppointmentDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
+const incomeReducer = (acc: Transaction, cur: Transaction) => {
+  if (cur.type === 'income') acc.value += cur.value;
+  return acc;
+};
+
+const outcomeReducer = (acc: Transaction, cur: Transaction) => {
+  if (cur.type === 'outcome') acc.value += cur.value;
+  return acc;
+};
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +30,27 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
+    const incomeTransactions = this.transactions.reduce(incomeReducer);
+    const outcomeTransactions = this.transactions.reduce(outcomeReducer);
+
+    const balance: Balance = {
+      income: incomeTransactions.value,
+      outcome: outcomeTransactions.value,
+      total: incomeTransactions.value - outcomeTransactions.value,
+    };
+
+    return balance;
     // TODO
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, type, value }: CreateAppointmentDTO): Transaction {
+    const transaction = new Transaction({ title, type, value });
+    this.transactions.push(transaction);
+    return transaction;
   }
 }
 
--- src/routes/transaction.routes.ts
+++ src/routes/transaction.routes.ts
@@ -1,23 +1,36 @@
 import { Router } from 'express';
 
-// import TransactionsRepository from '../repositories/TransactionsRepository';
-// import CreateTransactionService from '../services/CreateTransactionService';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import CreateTransactionService from '../services/CreateTransactionService';
+import GetTransactionService from '../services/GetTransactionsService';
 
 const transactionRouter = Router();
 
-// const transactionsRepository = new TransactionsRepository();
+const transactionsRepository = new TransactionsRepository();
+const createTransactionService = new CreateTransactionService(
+  transactionsRepository,
+);
 
-transactionRouter.get('/', (request, response) => {
+const getTransactionService = new GetTransactionService(transactionsRepository);
+
+transactionRouter.get('/transactions', (request, response) => {
   try {
-    // TODO
+    const transactions = getTransactionService.execute();
+    return response.status(200).json(transactions);
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
 });
 
-transactionRouter.post('/', (request, response) => {
+transactionRouter.post('/transactions', (request, response) => {
   try {
-    // TODO
+    const { title, value, type } = request.body;
+    const transaction = createTransactionService.execute({
+      title,
+      value,
+      type,
+    });
+    return response.status(200).json(transaction);
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
